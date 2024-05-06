332e1c4e95fc72de2650541ec678a037823e6e97
Johannes Franke <70877571+JohannesF99@users.noreply.github.com>
2024-05-06 18:47:32 +0000

    Optimize ML approach

--- language-identification-stopwords/language_identification_stopwords.py
+++ language-identification-stopwords/language_identification_stopwords.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from joblib import load
+import pandas as pd
 from tira.rest_api_client import Client
 from tira.third_party_integrations import get_output_directory
 
@@ -8,10 +9,23 @@ def print_accuracy(target_validation, text_validation):
     merged_df = pd.merge(target_validation, text_validation, on='id', suffixes=('_gt', '_pred'))
 
     # Compare the 'lang' column from ground truth and prediction
-    correct_predictions = merged_df[merged_df['lang_gt'] == merged_df['lang_pred']]
+    correct_predictions = merged_df[merged_df['lang_gt'] != merged_df['lang_pred']]
+    
+    # Zähle die Anzahl der falsch klassifizierten Vorhersagen für jeden Sprachcode
+    wrong_predictions_counts = correct_predictions[correct_predictions['lang_gt'] != correct_predictions['lang_pred']]['lang_gt'].value_counts()
+
+    # Berechne den Prozentsatz der falsch klassifizierten Vorhersagen für jeden Sprachcode
+    total_wrong_predictions = len(correct_predictions[correct_predictions['lang_gt'] != correct_predictions['lang_pred']])
+    error_rates = wrong_predictions_counts / total_wrong_predictions * 100
+
+    # Sortiere die Sprachcodes nach ihrem Fehleranteil
+    error_rates_sorted = error_rates.sort_values()
+
+    print("Fehleranteil nach Sprachcode:")
+    print(error_rates_sorted)
 
     # Calculate accuracy
-    accuracy = len(correct_predictions) / len(merged_df)
+    accuracy = 1 - len(correct_predictions) / len(merged_df)
     print("Accuracy:", accuracy)
 
 if __name__ == "__main__":
--- language-identification-stopwords/train.py
+++ language-identification-stopwords/train.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import pandas as pd
 from joblib import dump
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
@@ -20,13 +20,21 @@ if __name__ == "__main__":
         "nlpbuw-fsu-sose-24", "language-identification-validation-20240429-training"
     )
     df = text.join(labels.set_index("id"))
+    remain = df[1000:]
     df = df.head(1000)
+
+    # Trainiere mehr auf Texten mit Sprachen, die schlechter performen
+    zh_df = remain.query('lang == "zh"')
+    no_df = remain.query('lang == "no"')
+    da_df = remain.query('lang == "da"')
+
+    optimized_df = pd.concat([df, no_df.head(200), zh_df.head(1000), da_df.head(200)])
 
     # Train the model
     model = Pipeline(
         [("vectorizer", CountVectorizer()), ("classifier", MultinomialNB())]
     )
-    model.fit(df["text"], df["lang"])
+    model.fit(optimized_df["text"], optimized_df["lang"])
 
     # Save the model
     dump(model, Path(__file__).parent / "model.joblib")
